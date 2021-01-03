a2b1dadedd205142e0f674c5cc9611a23ed4ba77
jamwa <jamwa@LAPTOP-AOT80K1O>
2021-01-03 14:23:22 +0530

    Read cell value

---
+++ AutomationProject/src/test/java/org/testing/utilities/ExcelRead.java
@@ -0,0 +1,22 @@
+package org.testing.utilities;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+
+public class ExcelRead {
+	
+	public static String Read(String FilePath, int sheet, int row, int col) throws BiffException, IOException
+	{
+		File f = new File(FilePath);
+		Workbook wb = Workbook.getWorkbook(f);
+		Sheet s = wb.getSheet(sheet);
+		String data = s.getCell(col,row).getContents();
+		return data;
+	}
+
+}
